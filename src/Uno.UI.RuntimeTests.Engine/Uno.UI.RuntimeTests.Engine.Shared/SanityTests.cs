5c8f729b1292d1712c372bd290d33d428e6bbb94
xiaoy312 <xiaoyao312@gmail.com>
2022-10-06 11:50:26 -0400

    test: sanity tests

---
+++ src/Uno.UI.RuntimeTests.Engine/Uno.UI.RuntimeTests.Engine.Shared/SanityTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Uno.UI.RuntimeTests.Engine
+{
+	[TestClass]
+	public class SanityTests
+	{
+		[TestMethod]
+		public void Is_Sane()
+		{
+		}
+
+		[TestMethod]
+		public async Task Is_Still_Sane()
+		{
+			await Task.Delay(2000);
+		}
+
+#if DEBUG
+		[TestMethod]
+		public async Task No_Longer_Sane() // expected to fail
+		{
+			await Task.Delay(2000);
+
+			throw new Exception("Great works require a touch of insanity.");
+		}
+
+		[TestMethod, Ignore]
+		public void Is_An_Illusion() // expected to be ignored
+		{
+		}
+#endif
+	}
+}
