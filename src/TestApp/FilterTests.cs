a11b59456b56429862b1216dde3d913b3a95e9ef
Jerome Laban <jerome@platform.uno>
2026-01-15 10:46:38 -0500

    chore: Adjust for text exclusions

--- src/TestApp/FilterTests.cs
+++ src/TestApp/FilterTests.cs
@@ -67,6 +67,18 @@ public class FilterTests
 	[DataRow("!abc | defg", "abc.def.g.h.i", false)]
 	[DataRow("abcd | !def", "abc.def.g.h.i", false)]
 	[DataRow("!abcd | def", "abc.def.g.h.i", true)]
+
+	// Chained NOT filters (used in CI to exclude multiple test classes)
+	[DataRow("!abc & !def", "abc.test", false)]
+	[DataRow("!abc & !def", "def.test", false)]
+	[DataRow("!abc & !def", "ghi.test", true)]
+	[DataRow("!abc & !def & !ghi", "abc.test", false)]
+	[DataRow("!abc & !def & !ghi", "def.test", false)]
+	[DataRow("!abc & !def & !ghi", "ghi.test", false)]
+	[DataRow("!abc & !def & !ghi", "xyz.test", true)]
+	[DataRow("!HotReloadTests & !SecondaryAppTests", "Uno.UI.RuntimeTests.Engine.HotReloadTests.Is_CodeHotReload_Enabled", false)]
+	[DataRow("!HotReloadTests & !SecondaryAppTests", "Uno.UI.RuntimeTests.Engine.SecondaryAppTests.Is_From_A_Secondary_App", false)]
+	[DataRow("!HotReloadTests & !SecondaryAppTests", "Uno.UI.RuntimeTests.Engine.SanityTests.Is_Sane", true)]
 	public void When_ParseAndMatch(string filter, string method, bool expectedResult)
 	{
 		UnitTestFilter sut = filter;
